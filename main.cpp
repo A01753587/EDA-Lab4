41b5692b52d3f4c621c3b8646adf494c163415a5
A01802469 <A01802469@tec.mx>
2025-11-07 00:37:33 -0600

    final, falta merge

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<algorithm>
 
 // Print function
+std::vector<int> data;
 std::ostream& operator<<(std::ostream& out, std::vector<int> &vect){
     out<<"[";
     bool first_time {true};
@@ -23,7 +24,7 @@ void fill_vector(){
     std::cout<<"\nSize of vector: ";
     std::cin>>n;
     data.resize(n);
-    std::cout<<"\n1. Random\n2. Incremental\n 3. Decremental\n\t";
+    std::cout<<"\n1. Random\n2. Incremental\ngit 3. Decremental\n\t";
     std::cin>>opcInit;
     switch (opcInit)
     {
@@ -42,7 +43,6 @@ void fill_vector(){
     }
 }
 
-std::vector<int> data;
 
 int main(){
 
@@ -50,51 +50,63 @@ int main(){
     int opc;
 
     do{
+        double total_time;
         std::cout<<"\n1. Selectionsort\n2. Insertionsort\n3. Bubblesort\n4. Quicksort\n5. Mergesort\n6. Countingsort\n7. Funcion sorting de la libería estándar\n\n\t";
         std::cin>>opc;
         switch (opc)
         {
         case 1:
+        {
             fill_vector();
             auto start_selection = std::chrono::high_resolution_clock::now();
             sort.selectionsort(data);
             auto stop_selection = std::chrono::high_resolution_clock::now();
             auto duration_selection = std::chrono::duration_cast<std::chrono::microseconds>(stop_selection - start_selection);
-            double total_time_selection = duration_selection.count() / 1'000'000.0;
+            total_time = duration_selection.count() / 1'000'000.0;
+        }
             break;
         case 2:
+        {
             fill_vector();
             auto start_insertion = std::chrono::high_resolution_clock::now();
             sort.insertionsort(data);
             auto stop_insertion = std::chrono::high_resolution_clock::now();
-            auto duration_insertion = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-            double total_time_insertion = duration_insertion.count() / 1'000'000.0;
+            auto duration_insertion = std::chrono::duration_cast<std::chrono::microseconds>(stop_insertion - start_insertion);
+            total_time = duration_insertion.count() / 1'000'000.0;
+        }
             break;
         case 3:
+        {
             fill_vector();
             auto start_bubble = std::chrono::high_resolution_clock::now();
             sort.bubblesort(data);
             auto stop_bubble = std::chrono::high_resolution_clock::now();
-            auto duration_bubble = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-            double total_time_bubble = duration_bubble.count() / 1'000'000.0;
+            auto duration_bubble = std::chrono::duration_cast<std::chrono::microseconds>(stop_bubble - start_bubble);
+            total_time = duration_bubble.count() / 1'000'000.0;
+        }
             break;
         case 4:
+        {
             fill_vector();
             auto start_quick = std::chrono::high_resolution_clock::now();
             sort.quicksort(data,0,data.size());
             auto stop_quick = std::chrono::high_resolution_clock::now();
-            auto duration_quick = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-            double total_time_quick = duration_quick.count() / 1'000'000.0;
+            auto duration_quick = std::chrono::duration_cast<std::chrono::microseconds>(stop_quick - start_quick);
+            total_time = duration_quick.count() / 1'000'000.0;
+        }
             break;
         case 5:
+        {
             fill_vector();
             auto start_merge = std::chrono::high_resolution_clock::now();
-            sort.mergesort(data);
+            // sort.mergesort(data);
             auto stop_merge = std::chrono::high_resolution_clock::now();
-            auto duration_merge = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-            double total_time_merge = duration_merge.count() / 1'000'000.0;
+            auto duration_merge = std::chrono::duration_cast<std::chrono::microseconds>(stop_merge - start_merge);
+            total_time = duration_merge.count() / 1'000'000.0;
+        }
             break;
         case 6:
+        {
             fill_vector();
             auto iterator=std::max_element(data.begin(),data.end());
             int max_value= *iterator;
@@ -102,17 +114,21 @@ int main(){
             sort.countingsort(data,max_value);
             auto stop_counting = std::chrono::high_resolution_clock::now();
             auto duration_counting = std::chrono::duration_cast<std::chrono::microseconds>(stop_counting - start_counting);
-            double total_time_counting = duration_counting.count() / 1'000'000.0;
+            total_time = duration_counting.count() / 1'000'000.0;
+        }
             break;
         case 7:
+        {
             fill_vector();
             auto start_sort = std::chrono::high_resolution_clock::now();
             std::sort(data.begin(),data.end());
             auto stop_sort = std::chrono::high_resolution_clock::now();
             auto duration_sort = std::chrono::duration_cast<std::chrono::microseconds>(stop_sort - start_sort);
-            double total_time_sort = duration_sort.count() / 1'000'000.0;
+            total_time = duration_sort.count() / 1'000'000.0;
+        }
             break;    
         }
+        std::cout<<"El tiempo total de ejecucion es de: "<<total_time<<" segundos\n";
         if (std::is_sorted(data.begin(), data.end())) std::cout << "The vector is sorted.\n";
         else std::cout << "The vector is not sorted.\n";
         data.clear();
